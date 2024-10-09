ec86f244a0e405fbccb3cb5dae9b2e66cfd966d6
TioMarreco <pedroboy1204@gmail.com>
2024-10-09 14:24:57 -0300

    Add files via upload

---
+++ Projeto-de-Dados-Museum/Projeto_Museu_de_Dados/FrontEnd/Front.py
@@ -0,0 +1,14 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from BackEnd.EstatuasBE import *
+from BackEnd.UsuárioBE import *
+from BackEnd.QuadrosBE import *
+from BackEnd.Login import *
+import customtkinter as ctk
+
+
+
+
+
+
