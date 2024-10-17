651fd4e57c80532844d4d04e24d1841319c2911b
TioMarreco <pedroboy1204@gmail.com>
2024-10-17 17:49:55 -0300

    Add files via upload

--- Projeto_Museu_de_Dados/Main.py
+++ Projeto_Museu_de_Dados/Main.py
@@ -2,19 +2,20 @@ import customtkinter as ctk
 import psycopg2
 from tkinter import messagebox
 
+conn = psycopg2.connect(
+    dbname="20221214010013",
+     user="postgres",
+     password="pabd",
+     host="localhost"
+)
+cursor = conn.cursor()
+
 # Função de login
 def login():
-    usuario_nome = entry_username.get()
+    usuario_nome = entry_usuario_nome.get()
     senha = entry_password.get()
 
     try:
-        conn = psycopg2.connect(
-            dbname="20221214010013",
-            user="postgres",
-            password="pabd",
-            host="localhost"
-        )
-        cursor = conn.cursor()
 
         cursor.execute("SELECT * FROM usuarios WHERE usuario_nome=%s AND senha=%s", (usuario_nome, senha))
         user = cursor.fetchone()
@@ -29,19 +30,43 @@ def login():
     except Exception as e:
         messagebox.showerror("Erro", f"Erro de conexão: {e}")
 
-def abrir_tela_principal(conn):
+
+# Configurações da janela
+ctk.set_appearance_mode("dark")  # Modo escuro
+ctk.set_default_color_theme("blue")  # Tema azul
+
+root = ctk.CTk()  # Criação da janela
+root.title("Tela de Login")
+root.geometry('300x300')
+
+# Widgets da tela de login
+label_usuario_nome = ctk.CTkLabel(root, text="Usuário")
+label_usuario_nome.pack(pady=10)
+
+entry_usuario_nome = ctk.CTkEntry(root)
+entry_usuario_nome.pack(pady=10)
+
+label_password = ctk.CTkLabel(root, text="Senha")
+label_password.pack(pady=10)
+
+entry_password = ctk.CTkEntry(root, show="*")
+entry_password.pack(pady=10)
+
+button_login = ctk.CTkButton(root, text="Login", command=login)
+button_login.pack(pady=20)
+
+def abrir_tela_principal():
     root.withdraw()  # Oculta a janela de login
     root.geometry('300x300')
     tela_principal = ctk.CTkToplevel(root)
     tela_principal.title("Tela Principal")
-
     def criar_usuario():
         def salvar_usuario():
             novo_usuario = entry_novo_usuario.get()
             nova_senha = entry_nova_senha.get()
             try:
                 cursor = conn.cursor()
-                cursor.execute("INSERT INTO usuarios (username, password) VALUES (%s, %s)", (novo_usuario, nova_senha))
+                cursor.execute("INSERT INTO usuarios (H, password) VALUES (%s, %s)", (novo_usuario, nova_senha))
                 conn.commit()
                 messagebox.showinfo("Sucesso", "Usuário criado com sucesso!")
                 entry_novo_usuario.delete(0, ctk.END)
@@ -68,7 +93,7 @@ def abrir_tela_principal(conn):
     def ler_usuarios():
         try:
             cursor = conn.cursor()
-            cursor.execute("SELECT username FROM usuarios;")
+            cursor.execute("SELECT usuario_nome FROM usuarios;")
             usuarios = cursor.fetchall()
             usuarios_lista = "\n".join([user[0] for user in usuarios])
             messagebox.showinfo("Usuários", f"Usuários:\n{usuarios_lista}")
@@ -81,14 +106,14 @@ def abrir_tela_principal(conn):
             usuario_a_deletar = entry_usuario_deletar.get()
             try:
                 cursor = conn.cursor()
-                cursor.execute("DELETE FROM usuarios WHERE username=%s", (usuario_a_deletar,))
+                cursor.execute("DELETE FROM usuarios WHERE usuario_nome=%s", (usuario_a_deletar,))
                 conn.commit()
                 messagebox.showinfo("Sucesso", "Usuário deletado com sucesso!")
                 entry_usuario_deletar.delete(0, ctk.END)
                 cursor.close()
             except Exception as e:
                 messagebox.showerror("Erro", f"Erro ao deletar usuário: {e}")
-
+        
         label_usuario_deletar = ctk.CTkLabel(tela_deletar_usuario, text="Usuário a Deletar:")
         label_usuario_deletar.pack(pady=5)
 
@@ -119,30 +144,11 @@ def abrir_tela_principal(conn):
 
     # Mostra as telas apropriadas
     button_criar.configure(command=lambda: (tela_criar_usuario.deiconify(), tela_deletar_usuario.withdraw()))
-    button_deletar.configure(command=lambda: (tela_deletar_usuario.deiconify(), tela_criar_usuario.withdraw()))
+    #button_deletar.configure(command=lambda: (tela_deletar_usuario.deiconify(), tela_criar_usuario.withdraw()))
 
-# Configurações da janela
-ctk.set_appearance_mode("dark")  # Modo escuro
-ctk.set_default_color_theme("blue")  # Tema azul
+button_editar = ctk.CTkButton(root, text='Editar usuarios', command=abrir_tela_principal)
+button_editar.pack(pady=15)
 
-root = ctk.CTk()  # Criação da janela
-root.title("Tela de Login")
-
-# Widgets da tela de login
-label_username = ctk.CTkLabel(root, text="Usuário")
-label_username.pack(pady=10)
-
-entry_username = ctk.CTkEntry(root)
-entry_username.pack(pady=10)
-
-label_password = ctk.CTkLabel(root, text="Senha")
-label_password.pack(pady=10)
-
-entry_password = ctk.CTkEntry(root, show="*")
-entry_password.pack(pady=10)
-
-button_login = ctk.CTkButton(root, text="Login", command=login)
-button_login.pack(pady=20)
 
 # Iniciar o loop principal
 root.mainloop()
